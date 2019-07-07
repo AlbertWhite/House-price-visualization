582e7432a353e21f10e412357aec933514b3adf8
yuebaiXu <xyb1114@gmail.com>
2019-07-07 21:43:41 +0200

    fixup

--- webpack.prod.js
+++ webpack.prod.js
@@ -26,7 +26,7 @@ module.exports = {
   },
   output: {
     filename: 'index.bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'doc')
   },
   plugins: [
     new UglifyJSPlugin({ sourceMap: true }),
